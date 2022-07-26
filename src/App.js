13bbab076cd6a06fcd0b8a1f03861d8ecfe24bf7
Ana Ribeiro <anabrazribeiro@gmail.com>
2022-07-26 08:30:42 +0100

    Created search engine

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
@@ -8,6 +9,23 @@ export default function App() {
         <header className="App-header">
           <img src={logo} alt="logo" />
         </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            This project was coded by Ana Ribeiro and it is{" "}
+            <span>
+              <a
+                href="https://github.com/AnaBRibeiro/dictionary-react-app"
+                target="_blank"
+                rel="noreferrer"
+              >
+                open-sourced on GitHub
+              </a>
+            </span>
+          </small>
+        </footer>
       </div>
     </div>
   );
