a529916bab61fce45cd7f9d21c5b79703f654dd1
Ana Ribeiro <anabrazribeiro@gmail.com>
2022-07-29 13:59:47 +0100

    Photos API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,32 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
-export default function Dictionary() {
+export default function Dictionary(props) {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
   }
 
-  function searchWord(event) {
+  function handlePhotosResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
+  function search(event) {
     event.preventDefault();
 
     let dictionaryApiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(dictionaryApiUrl).then(handleResponse);
+    axios.get(dictionaryApiUrl).then(handleDictionaryResponse);
+
+    let photosApiKey = `563492ad6f91700001000001b73422a8115d45fd98fe4e8848291256`;
+    let numberOfPhotos = 6;
+    let photosApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=${numberOfPhotos}`;
+    let headers = { Authorization: `Bearer ${photosApiKey}` };
+    axios.get(photosApiUrl, { headers: headers }).then(handlePhotosResponse);
   }
 
   function handleKeywordChange(event) {
@@ -23,10 +35,11 @@ export default function Dictionary() {
 
   return (
     <div className="Dictionary">
-      <form onSubmit={searchWord}>
+      <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
